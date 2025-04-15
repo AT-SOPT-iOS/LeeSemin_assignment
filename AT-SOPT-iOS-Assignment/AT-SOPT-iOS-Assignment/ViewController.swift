8d9182da2f4ee6bf9c4f773e424affe664478f34
sem-git <lsmsm0327@naver.com>
2025-04-15 10:51:47 +0900

    chore: 프로젝트 생성 및 초기 세팅

---
+++ AT-SOPT-iOS-Assignment/AT-SOPT-iOS-Assignment/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AT-SOPT-iOS-Assignment
+//
+//  Created by 이세민 on 4/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .blue
+    }
+
+}
