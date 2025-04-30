4a1ad9d715f05085266408c289dcba76a2334b73
sem-git <lsmsm0327@naver.com>
2025-05-01 04:25:11 +0900

    feat: TodaysTop20Cell 구현

---
+++ AT-SOPT-iOS-Assignment/AT-SOPT-iOS-Assignment/Presentation/Main/Cell/TodaysTop20Cell.swift
@@ -0,0 +1,60 @@
+//
+//  TodaysTop20Cell.swift
+//  AT-SOPT-iOS-Assignment
+//
+//  Created by 이세민 on 5/1/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class TodaysTop20Cell: UICollectionViewCell {
+    
+    private let numberLabel = UILabel().then {
+        $0.font = .semiBold(size: 50)
+        $0.textColor = .white
+        $0.transform = CGAffineTransform(rotationAngle: 0.15)
+    }
+    
+    private let posterImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.layer.cornerRadius = 3
+        $0.clipsToBounds = true
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        addSubviews(numberLabel, posterImageView)
+    }
+    
+    private func setLayout() {
+        numberLabel.snp.makeConstraints {
+            $0.bottom.equalTo(posterImageView.snp.bottom)
+            $0.leading.equalToSuperview().offset(8)
+        }
+        
+        posterImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalTo(numberLabel.snp.trailing).offset(10)
+            $0.width.equalTo(98)
+            $0.height.equalTo(146)
+        }
+    }
+    
+    func configure(_ contentList: ContentList) {
+        numberLabel.text = "\(contentList.rank)"
+        posterImageView.image = contentList.logoImage
+    }
+}
