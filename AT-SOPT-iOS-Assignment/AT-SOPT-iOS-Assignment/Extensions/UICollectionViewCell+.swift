03e2e155caad3b95e513351d7c9f0f27ed135459
sem-git <lsmsm0327@naver.com>
2025-05-01 04:25:28 +0900

    refactor: UICollectionViewCell+ 추가

---
+++ AT-SOPT-iOS-Assignment/AT-SOPT-iOS-Assignment/Extensions/UICollectionViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+.swift
+//  AT-SOPT-iOS-Assignment
+//
+//  Created by 이세민 on 5/1/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier : String {
+        return String(describing: self)
+    }
+}
