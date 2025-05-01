0b43b332d99da8c38314649fe5ee3cfe0edebc54
sem-git <lsmsm0327@naver.com>
2025-05-01 18:49:56 +0900

    feat: RealTimePopularLiveCell 구현

---
+++ AT-SOPT-iOS-Assignment/AT-SOPT-iOS-Assignment/Presentation/Main/Cell/RealTimePopularLiveCell.swift
@@ -0,0 +1,93 @@
+//
+//  RealTimePopularLiveCell.swift
+//  AT-SOPT-iOS-Assignment
+//
+//  Created by 이세민 on 5/1/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class RealTimePopularLiveCell: UICollectionViewCell {
+    
+    private let posterImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.layer.cornerRadius = 3
+        $0.clipsToBounds = true
+    }
+    
+    private let rankLabel = UILabel().then {
+        $0.font = .semiBold(size: 19)
+        $0.textColor = .white
+        $0.transform = CGAffineTransform(rotationAngle: 0.15)
+    }
+   
+    private let titleLabel = UILabel().then {
+        $0.font = .medium(size: 10)
+        $0.textColor = .white
+    }
+    
+    private let episodeLabel = UILabel().then {
+        $0.font = .regular(size: 10)
+        $0.textColor = .gray2
+    }
+    
+    private let ratingsLabel = UILabel().then {
+        $0.font = .regular(size: 10)
+        $0.textColor = .gray2
+    }
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        addSubviews(posterImageView, rankLabel, titleLabel, episodeLabel, ratingsLabel)
+    }
+    
+    private func setLayout() {
+        posterImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.width.equalTo(160)
+            $0.height.equalTo(80)
+        }
+        
+        rankLabel.snp.makeConstraints {
+            $0.top.equalTo(posterImageView.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(rankLabel.snp.top).offset(2)
+            $0.leading.equalTo(rankLabel.snp.trailing).offset(6)
+        }
+        
+        episodeLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(rankLabel.snp.trailing).offset(7)
+        }
+        
+        ratingsLabel.snp.makeConstraints {
+            $0.top.equalTo(episodeLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(rankLabel.snp.trailing).offset(7)
+        }
+    }
+    
+    func configure(_ content: RealTimePopularLiveContent) {
+        posterImageView.image = content.posterImage
+        rankLabel.text = "\(content.rank)"
+        titleLabel.text = content.title
+        episodeLabel.text = content.episode
+        ratingsLabel.text = content.ratings
+    }
+}
