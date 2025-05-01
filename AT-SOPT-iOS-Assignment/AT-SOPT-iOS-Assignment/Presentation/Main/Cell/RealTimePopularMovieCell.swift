d4f84457da52b47b0b001d96ea0551043adfb3bd
sem-git <lsmsm0327@naver.com>
2025-05-02 00:42:28 +0900

    feat: RealTimePopularMovieCell 구현

---
+++ AT-SOPT-iOS-Assignment/AT-SOPT-iOS-Assignment/Presentation/Main/Cell/RealTimePopularMovieCell.swift
@@ -0,0 +1,48 @@
+//
+//  RealTimePopularMovieCell.swift
+//  AT-SOPT-iOS-Assignment
+//
+//  Created by 이세민 on 5/2/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class RealTimePopularMovieCell: UICollectionViewCell {
+    
+    private let posterImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.layer.cornerRadius = 3
+        $0.clipsToBounds = true
+    }
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        addSubviews(posterImageView)
+    }
+    
+    private func setLayout() {
+        posterImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(8)
+            $0.width.equalTo(98)
+            $0.height.equalTo(146)
+        }
+    }
+    
+    func configure(_ content: RealTimePopularMovieContent) {
+        posterImageView.image = content.posterImage
+    }
+}
