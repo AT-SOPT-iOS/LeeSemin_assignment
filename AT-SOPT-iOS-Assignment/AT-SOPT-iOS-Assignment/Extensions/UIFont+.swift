133e90e7afa22ade81f1d4b737a9e33339ebf98b
sem-git <lsmsm0327@naver.com>
2025-06-04 19:29:12 +0900

    refactor: 동일 폰트 사용 가능하도록 SwiftUI Font Extension 추가

--- AT-SOPT-iOS-Assignment/AT-SOPT-iOS-Assignment/Extensions/UIFont+.swift
+++ AT-SOPT-iOS-Assignment/AT-SOPT-iOS-Assignment/Extensions/UIFont+.swift
@@ -6,22 +6,49 @@
 //
 
 import UIKit
+import SwiftUI
+
+enum FontName {
+    static let regular = "Pretendard-Regular"
+    static let medium = "Pretendard-Medium"
+    static let semiBold = "Pretendard-SemiBold"
+    static let bold = "Pretendard-Bold"
+}
 
 extension UIFont {
     
-    static func medium(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
-    }
-    
     static func regular(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
+            return UIFont(name: FontName.regular, size: size) ?? .systemFont(ofSize: size)
+        }
+        
+        static func medium(size: CGFloat) -> UIFont {
+            return UIFont(name: FontName.medium, size: size) ?? .systemFont(ofSize: size)
+        }
+        
+        static func semiBold(size: CGFloat) -> UIFont {
+            return UIFont(name: FontName.semiBold, size: size) ?? .systemFont(ofSize: size)
+        }
+        
+        static func bold(size: CGFloat) -> UIFont {
+            return UIFont(name: FontName.bold, size: size) ?? .boldSystemFont(ofSize: size)
+        }
+}
+
+extension Font {
+    
+    static func regular(size: CGFloat) -> Font {
+        return .custom(FontName.regular, size: size)
     }
     
-    static func semiBold(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: size) ?? UIFont.systemFont(ofSize: size)
+    static func medium(size: CGFloat) -> Font {
+        return .custom(FontName.medium, size: size)
     }
     
-    static func bold(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Bold", size: size) ?? UIFont.boldSystemFont(ofSize: size)
+    static func semiBold(size: CGFloat) -> Font {
+        return .custom(FontName.semiBold, size: size)
+    }
+    
+    static func bold(size: CGFloat) -> Font {
+        return .custom(FontName.bold, size: size)
     }
 }
